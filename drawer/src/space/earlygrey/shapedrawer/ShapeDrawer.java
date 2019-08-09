9af2ce4a1e0ebe123a7c72bcd688704fb78ab2d3
earlygrey <contact@earlygrey.space>
2019-08-10 00:32:56 +0200

    reduced number of sides estimated for ellipse

--- drawer/src/space/earlygrey/shapedrawer/ShapeDrawer.java
+++ drawer/src/space/earlygrey/shapedrawer/ShapeDrawer.java
@@ -322,10 +322,10 @@ public class ShapeDrawer extends AbstractShapeDrawer {
      */
     public void ellipse(float centreX, float centreY, float radiusX, float radiusY, float rotation, float lineWidth, JoinType joinType) {
         float circumference = (float) (MathUtils.PI2 * Math.sqrt((radiusX*radiusX + radiusY*radiusY)/2f));
-        int sides = (int) (circumference / (10*pixelSize));
+        int sides = (int) (circumference / (24 * pixelSize));
         float a = Math.min(radiusX, radiusY), b = Math.max(radiusX, radiusY);
         float eccentricity = (float) Math.sqrt(1-((a*a) / (b*b)));
-        sides += (sides * eccentricity) / 2;
+        sides += (sides * eccentricity) / 16;
         polygon(centreX, centreY, sides, radiusX, radiusY, rotation, lineWidth, joinType);
     }
 
