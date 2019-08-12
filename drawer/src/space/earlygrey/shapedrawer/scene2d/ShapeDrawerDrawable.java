3a55cd37d42120e7cfcace58936ee5e5f8a43d44
earlygrey <contact@earlygrey.space>
2019-08-12 10:38:58 +0200

    moved scene2d package into shapedrawer package (was accidentally in parent)

--- drawer/src/space/earlygrey/shapedrawer/scene2d/ShapeDrawerDrawable.java
+++ drawer/src/space/earlygrey/shapedrawer/scene2d/ShapeDrawerDrawable.java
@@ -1,4 +1,4 @@
-package space.earlygrey.scene2d;
+package space.earlygrey.shapedrawer.scene2d;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.utils.BaseDrawable;
