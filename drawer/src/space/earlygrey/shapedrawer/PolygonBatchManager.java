c1d2c053fbc21ae9eebc9fb9e4e6cc4d10e5ed91
earlygrey <contact@earlygrey.space>
2019-09-26 00:32:37 +0200

    changed short[] triangles indexing to be in line with float[] vertices indexing

--- drawer/src/space/earlygrey/shapedrawer/PolygonBatchManager.java
+++ drawer/src/space/earlygrey/shapedrawer/PolygonBatchManager.java
@@ -6,9 +6,8 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class PolygonBatchManager extends BatchManager {
 
-    short[] triangles;
-    int triangleOffset = 0;
-    static final int TRIANGLE_PUSH_SIZE = 3 * VERTEX_SIZE;
+    protected short[] triangles;
+    protected int triangleCount = 0;
 
     PolygonBatchManager(PolygonBatch batch, TextureRegion region) {
         super(batch, region);
@@ -27,49 +26,47 @@ public class PolygonBatchManager extends BatchManager {
 
     @Override
     void pushQuad() {
-        triangles[triangleOffset++] = (short) (vertexCount + 0);
-        triangles[triangleOffset++] = (short) (vertexCount + 1);
-        triangles[triangleOffset++] = (short) (vertexCount + 2);
-        triangles[triangleOffset++] = (short) (vertexCount + 0);
-        triangles[triangleOffset++] = (short) (vertexCount + 2);
-        triangles[triangleOffset++] = (short) (vertexCount + 3);
+        pushTriangleIndices((short) vertexCount, (short) (vertexCount+1), (short) (vertexCount+2));
+        pushTriangleIndices((short) vertexCount, (short) (vertexCount+2), (short) (vertexCount+3));
         super.pushQuad();
     }
 
 
     @Override
     void pushTriangle() {
-        triangles[triangleOffset++] = (short) vertexCount;
-        triangles[triangleOffset++] = (short) (vertexCount+1);
-        triangles[triangleOffset++] = (short) (vertexCount+2);
-        int i = getVerticesArrayIndex();
-        verts[i + SpriteBatch.C1] = floatBits;
-        verts[i + SpriteBatch.C2] = floatBits;
-        verts[i + SpriteBatch.C3] = floatBits;
+        pushTriangleIndices((short) vertexCount, (short) (vertexCount+1), (short) (vertexCount+2));
+        int v = getVerticesArrayIndex();
+        verts[v + SpriteBatch.C1] = floatBits;
+        verts[v + SpriteBatch.C2] = floatBits;
+        verts[v + SpriteBatch.C3] = floatBits;
         vertexCount += 3;
     }
 
     void pushTriangleIndices(short t1, short t2, short t3) {
-        triangles[triangleOffset++] = t1;
-        triangles[triangleOffset++] = t2;
-        triangles[triangleOffset++] = t3;
+        int t = getTrianglesArrayOffset();
+        triangles[t] = t1;
+        triangles[t+1] = t2;
+        triangles[t+2] = t3;
+        triangleCount++;
     }
 
-    protected void pushVertexData(float[] vertices, short[] triangles, int trianglesCount)  {
+    protected void pushVertexData(float[] vertices, short[] triangles, int trianglesArrayCount)  {
 
-        int i = getVerticesArrayIndex();
+        int v = getVerticesArrayIndex();
 
         for (int j = 0; j < vertices.length; j+=2) {
             float x = vertices[j], y = vertices[j+1];
-            verts[i + SpriteBatch.X1] = x;
-            verts[i + SpriteBatch.Y1] = y;
-            verts[i + SpriteBatch.C1] = floatBits;
-            i += VERTEX_SIZE;
+            verts[v + SpriteBatch.X1] = x;
+            verts[v + SpriteBatch.Y1] = y;
+            verts[v + SpriteBatch.C1] = floatBits;
+            v += VERTEX_SIZE;
         }
 
-        for (int j = 0, n = trianglesCount; j < n; j++) {
-            this.triangles[triangleOffset++] = (short) (vertexCount + triangles[j]);
+        int t = getTrianglesArrayOffset();
+        for (int j = 0, n = trianglesArrayCount; j < n; j++) {
+            this.triangles[t+j] = (short) (vertexCount + triangles[j]);
         }
+        this.triangleCount += trianglesArrayCount / 3;
 
         vertexCount += vertices.length / 2;
     }
@@ -86,10 +83,13 @@ public class PolygonBatchManager extends BatchManager {
     @Override
     void pushToBatch() {
         if (vertexCount == 0) return;
-        getBatch().draw(r.getTexture(), verts, 0, getVerticesArrayIndex(), triangles, 0, triangleOffset);
+        getBatch().draw(r.getTexture(), verts, 0, getVerticesArrayIndex(), triangles, 0, getTrianglesArrayOffset());
         vertexCount = 0;
-        triangleOffset = 0;
+        triangleCount = 0;
     }
 
+    int getTrianglesArrayOffset() {
+        return 3* triangleCount;
+    }
 
 }
