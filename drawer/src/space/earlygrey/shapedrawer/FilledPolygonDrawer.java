65ec1b2196a69c04ac63a6b47f1c315a57326d3a
earlygrey <contact@earlygrey.space>
2019-09-25 23:01:05 +0200

    SpriteBatch can draw filled polygons

--- drawer/src/space/earlygrey/shapedrawer/FilledPolygonDrawer.java
+++ drawer/src/space/earlygrey/shapedrawer/FilledPolygonDrawer.java
@@ -53,12 +53,91 @@ abstract class FilledPolygonDrawer<T extends BatchManager> extends DrawerTemplat
 
         @Override
         void polygon(float centreX, float centreY, int sides, float radiusX, float radiusY, float rotation, float startAngle, float radians) {
+            if (radians==0) return;
+            radians = Math.min(radians, ShapeUtils.PI2);
 
+            boolean wasCaching = batchManager.startCaching();
+
+            float angleInterval = MathUtils.PI2 / sides;
+            float endAngle = startAngle + radians;
+
+            float cos = (float) Math.cos(angleInterval), sin = (float) Math.sin(angleInterval);
+            float cosRot = (float) Math.cos(rotation), sinRot = (float) Math.sin(rotation);
+
+            int start = (int) Math.ceil(sides * (startAngle / ShapeUtils.PI2));
+            int end = (int) Math.floor(sides * (endAngle / ShapeUtils.PI2)) + 1;
+
+            if (ShapeUtils.epsilonEquals(start * angleInterval, startAngle)) start++;
+
+            B.set(1, 0).rotateRad(startAngle).scl(radiusX, radiusY);
+
+            int n = end-start;
+            if (n<2) {
+                //no "regular" segments, will never enter loop
+                batchManager.ensureSpaceForTriangle();
+                A.set(1, 0).rotateRad(startAngle).scl(radiusX, radiusY);
+                B.set(1, 0).rotateRad(endAngle).scl(radiusX, radiusY);
+                vert1(centreX, centreY);
+                x2(A.x*cosRot-A.y*sinRot  + centreX);
+                y2(A.x*sinRot+A.y*cosRot + centreY);
+                x3(B.x*cosRot-B.y*sinRot  + centreX);
+                y3(B.x*sinRot+B.y*cosRot + centreY);
+                batchManager.pushTriangle();
+            } else {
+                //prepare for regular segments
+                dir.set(1, 0).rotateRad(Math.min(start * angleInterval, endAngle));
+                C.set(dir).scl(radiusX, radiusY);
+            }
+
+
+            for (int i = 0; i < n-1; i++) {
+
+                A.set(B);
+                B.set(C);
+                if (i<n-2) {
+                    dir.set(dir.x * cos - dir.y * sin, dir.x * sin + dir.y * cos);
+                    C.set(dir).scl(radiusX, radiusY);
+                } else {
+                    C.set(1, 0).rotateRad(endAngle).scl(radiusX, radiusY);
+                }
+
+                if (i%2==0) {
+                    batchManager.ensureSpaceForQuad();
+                    vert1(centreX, centreY);
+                    x2(A.x*cosRot-A.y*sinRot  + centreX);
+                    y2(A.x*sinRot+A.y*cosRot + centreY);
+                    x3(B.x*cosRot-B.y*sinRot  + centreX);
+                    y3(B.x*sinRot+B.y*cosRot + centreY);
+                    x4(C.x*cosRot-C.y*sinRot  + centreX);
+                    y4(C.x*sinRot+C.y*cosRot + centreY);
+                    batchManager.pushQuad();
+                } else if (i==n-2) {
+                    //draw final triangle
+                    batchManager.ensureSpaceForTriangle();
+                    C.set(1, 0).rotateRad(endAngle).scl(radiusX, radiusY);
+                    vert1(centreX, centreY);
+                    x2(B.x*cosRot-B.y*sinRot  + centreX);
+                    y2(B.x*sinRot+B.y*cosRot + centreY);
+                    x3(C.x*cosRot-C.y*sinRot  + centreX);
+                    y3(C.x*sinRot+C.y*cosRot + centreY);
+                    batchManager.pushTriangle();
+                }
+            }
+
+            if (!wasCaching) batchManager.endCaching();
         }
 
         @Override
         void polygon(float[] vertices, short[] triangles, int trianglesCount) {
 
+            for (int i = 0; i < trianglesCount; i+=3) {
+                batchManager.ensureSpaceForTriangle();
+                vert1(vertices[2*triangles[i]], vertices[2*triangles[i]+1]);
+                vert2(vertices[2*triangles[i+1]], vertices[2*triangles[i+1]+1]);
+                vert3(vertices[2*triangles[i+2]], vertices[2*triangles[i+2]+1]);
+                batchManager.pushTriangle();
+            }
+            batchManager.pushToBatch();
         }
     }
 
@@ -91,8 +170,7 @@ abstract class FilledPolygonDrawer<T extends BatchManager> extends DrawerTemplat
             int vertexOffset = batchManager.getVerticesArrayIndex();
 
             //centre point - triangle index 0
-            x1(centreX);
-            y1(centreY);
+            vert1(centreX, centreY);
             batchManager.pushVertex();
 
             //first perimeter vertex (at start angle) - triangle index 1
