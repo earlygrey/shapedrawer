c7d7cdadd439eba064eca7a76bdc0972a032d2a4
Thomas <crossie@gmail.com>
2021-07-09 09:47:50 +0200

    Improve DefaultSideEstimator

--- drawer/src/space/earlygrey/shapedrawer/DefaultSideEstimator.java
+++ drawer/src/space/earlygrey/shapedrawer/DefaultSideEstimator.java
@@ -2,12 +2,35 @@ package space.earlygrey.shapedrawer;
 
 public class DefaultSideEstimator implements ISideEstimator {
 
+	/**
+	 * Minimum value returned by {@link #estimateSidesRequired(float, float, float)}
+	 */
+	protected int minimumSides; // 
+	/**
+	 * Maximum value returned by {@link #estimateSidesRequired(float, float, float)}
+	 */
+	protected int maximumSides;
+	/**
+	 * Multiply the number of sides return by this value. {@link #minimumSides} and {@link #maximumSides} are not affected
+	 */
+	protected float sideMultiplicator;
+
+	public DefaultSideEstimator() {
+		this(20, 16000, 1f);
+	}
+
+	public DefaultSideEstimator(int minimumSides, int maximumSides, float sideMultiplicator) {
+		this.minimumSides = minimumSides;
+		this.maximumSides = maximumSides;
+		this.sideMultiplicator = sideMultiplicator;
+	}
+
 	public int estimateSidesRequired(float pixelSize, float radiusX, float radiusY) {
 		float circumference = (float) (ShapeUtils.PI2 * Math.sqrt((radiusX * radiusX + radiusY * radiusY) / 2f));
 		int sides = (int) (circumference / (16 * pixelSize));
 		float a = Math.min(radiusX, radiusY), b = Math.max(radiusX, radiusY);
 		float eccentricity = (float) Math.sqrt(1 - ((a * a) / (b * b)));
-		sides += (sides * eccentricity) / 16;
-		return Math.max(sides, 20);
+		sides += (sides * eccentricity * sideMultiplicator) / 16;
+		return Math.min(Math.max(sides, minimumSides), maximumSides);
 	}
 }
